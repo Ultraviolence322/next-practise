44d790603ed1ac249016f8fdffe24e8a96313ac4
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-15 20:57:27 +0300

    added MainLayout in User page

--- pages/users/[uid].jsx
+++ pages/users/[uid].jsx
@@ -1,19 +1,21 @@
-import { useRouter } from "next/dist/client/router"
+import MainLayout from '../../components/MainLayout'
 import style from '../../styles/UserPage.module.scss'
 import Head from 'next/head'
 
-export default function UserPage() {
-  const routetInfo = useRouter()
-  console.log('routetInfo', routetInfo);
+export default function UserPage({user}) {
+
   return (
+    <MainLayout>
       <Head>
         <title>{user.name}</title>
       </Head>
-    <div className={style.user}>
-      <h1>
-        user {routetInfo.query.uid}
-      </h1> 
-    </div>
+
+      <div className={style.user}>
+        <h1>
+          user {user.name}
+        </h1> 
+      </div>
+    </MainLayout>
   )
 }
 
