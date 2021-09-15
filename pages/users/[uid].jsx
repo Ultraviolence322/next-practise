f93a73019915d3c474f7e3ec1dc3bb7c787fe679
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-15 20:56:22 +0300

    added meta in User page

--- pages/users/[uid].jsx
+++ pages/users/[uid].jsx
@@ -1,10 +1,14 @@
 import { useRouter } from "next/dist/client/router"
 import style from '../../styles/UserPage.module.scss'
+import Head from 'next/head'
 
 export default function UserPage() {
   const routetInfo = useRouter()
   console.log('routetInfo', routetInfo);
   return (
+      <Head>
+        <title>{user.name}</title>
+      </Head>
     <div className={style.user}>
       <h1>
         user {routetInfo.query.uid}
