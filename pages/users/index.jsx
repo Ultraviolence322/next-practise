0d57fa0e1ba17243397cffcb728d1022a4320029
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-15 20:57:47 +0300

    added server side rendering for Users page

--- pages/users/index.jsx
+++ pages/users/index.jsx
@@ -31,4 +31,15 @@ function Users() {
   )
 }
 
+export async function getStaticProps(context) {
+  const response = await  fetch('https://jsonplaceholder.typicode.com/users')
+  const users = await response.json()
+
+  return {
+    props: {
+      users
+    }
+  }
+}
+
 export default Users
