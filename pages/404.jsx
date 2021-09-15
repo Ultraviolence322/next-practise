8ffa748e5c2d57780e8c5e0c021e6e3fe8d1743a
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-15 20:55:03 +0300

    added 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainLayout from '../components/MainLayout'
+
+export default function Page404() {
+  return (
+    <MainLayout>
+      <h1>Моя кастомная страница с ошибкой</h1>
+    </MainLayout>
+  )
+}
